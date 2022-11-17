3a12d18ecd724fe83cfe0cc84427a54dacbb0462
pnquang2405 <pnquang2405@gmail.com>
2022-11-18 00:51:06 +0700

    config ELK

--- TakeFoodAPI/Program.cs
+++ TakeFoodAPI/Program.cs
@@ -1,6 +1,16 @@
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+ConfigureLogs();
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,3 +33,35 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+#region helper
+void ConfigureLogs()
+{
+    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    var configuration = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile(
+                            $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                            optional: true)
+                        .Build();
+
+    Log.Logger = new LoggerConfiguration()
+           .Enrich.FromLogContext()
+           .Enrich.WithExceptionDetails() 
+           .WriteTo.Debug()
+           .WriteTo.Console()
+           .WriteTo.Elasticsearch(ConfigureELK(configuration, env))
+           .CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureELK(IConfigurationRoot configuration, string env)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ELKConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{env?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+    };
+}
+
+#endregion
