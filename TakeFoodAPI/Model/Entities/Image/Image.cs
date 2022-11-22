d021cf822958c8e9c1c4a846f15a3dd0c241c70c
Văn Hữu An <58378623+vanhuuan89@users.noreply.github.com>
2022-11-22 07:56:19 +0700

    add db and create modle

---
+++ TakeFoodAPI/Model/Entities/Image/Image.cs
@@ -0,0 +1,8 @@
+﻿namespace TakeFoodAPI.Model.Entities.Image;
+
+public class Image:ModelMongoDB
+{
+    public string StoreId { get; set; }
+    public string CategoryId { get; set; }
+    public string Url { get; set; }
+}
---
+++ TakeFoodAPI/Model/Entities/WorkTime/WorkTime.cs
@@ -0,0 +1,10 @@
+﻿namespace TakeFoodAPI.Model.Entities.WorkTime;
+
+public class WorkTime:ModelMongoDB
+{
+    public string Storeid { get; set; }
+    public DateTime EndDay { get; set; }
+    public DateTime StartDay { get; set; }
+    public int OpenHour { get; set; }
+    public int CloseHour { get; set; }
+}
